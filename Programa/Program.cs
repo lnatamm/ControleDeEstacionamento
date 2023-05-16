86475676fcbc2fe5f1d155c89e0f0ef9d34f8922
Levi <lnatamm25@gmail.com>
2023-05-16 15:42:24 -0300

    Adicionando método "Main()" na classe

--- Programa/Program.cs
+++ Programa/Program.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Controle_de_Estacionamento.Programa;
+﻿namespace Controle_de_Estacionamento.Programa;
 
 internal class Program
 {
-
+    public static void Main(string[] args)
+    {
+        
+    }
 }
