e2db1aba2e0f43aa088ca950d67aae1eb0d925e2
Levi <lnatamm25@gmail.com>
2023-05-16 15:38:37 -0300

    Criando classe "GlobalUsings"

---
+++ Classes/GlobalUsings.cs
@@ -0,0 +1,12 @@
+﻿global using System;
+global using System.Collections.Generic;
+global using System.Linq;
+global using System.Text;
+global using System.Threading.Tasks;
+global using Controle_de_Estacionamento.Classes;
+
+namespace Controle_de_Estacionamento.Classes;
+
+internal class GlobalUsings
+{
+}
