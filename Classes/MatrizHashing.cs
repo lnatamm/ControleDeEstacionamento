8e1e9f57d2f3431b2b5891421627e6b268be235d
Levi <lnatamm25@gmail.com>
2023-05-18 15:46:13 -0300

    Adicionando lista com todas as posições preenchidas. Alterando "Hashing()". Alterando a maneira de inserção, atualmente está por endereçamento aberto por tentativa linear. Alterando o método "Clear()"

--- Classes/MatrizHashing.cs
+++ Classes/MatrizHashing.cs
@@ -3,11 +3,14 @@ public class MatrizHashing
 {
     private Carro[][] array;
 
+    private List<int[]> posicoesPreenchidas;
+
     private int count;
 
     public MatrizHashing(int m, int n)
     {
         array = new Carro[m][];
+        posicoesPreenchidas = new List<int[]>();
         for (int i = 0; i < m; i++)
         {
             array[i] = new Carro[n];
@@ -17,24 +20,37 @@ public class MatrizHashing
 
     private int Hashing(Carro data)
     {
-        return data.GetHashCode() % array.Length;
+        return data.GetHashCode() % array[0].Length;
     }
 
     public void Add(Carro data)
     {
         int hash = Hashing(data);
-        int index = hash;
-        int i = 1;
-        while (array[index / array[0].Length][index % array[0].Length] != null)
+        int linha = hash % array.Length;
+        int coluna = hash;
+        int tentativa = 1;
+        int tentativaLinha = 1;
+        int[] finalIndex;
+        while (array[linha][coluna] != null)
         {
-            index = (hash + (i * i)) % (array.Length * array[0].Length);
-            i++;
-            if(i > array.Length * array[0].Length)
+            coluna = (hash + tentativa) % array[linha].Length;
+            tentativa++;
+            if(tentativa > array[linha].Length)
             {
-                return;
+                tentativa = 1;
+                linha++;
+                tentativaLinha++;
+                if(tentativaLinha > array.Length)
+                {
+                    return;
+                }
+                linha = linha % array.Length;
+                coluna = hash;
             }
         }
-        array[index / array[0].Length][index % array[0].Length] = data;
+        finalIndex = new int[] {linha, coluna};
+        array[finalIndex[0]][finalIndex[1]] = data;
+        posicoesPreenchidas.Add(finalIndex);
     }
 
     public int Count()
@@ -42,32 +58,73 @@ public class MatrizHashing
         return count;
     }
 
+    private int InputTime()
+    {
+        int input = 0;
+        try
+        {
+            input = Convert.ToInt32(Console.ReadLine());
+            return input >= 0 ? input : InputTime();
+        }
+        catch
+        {
+            Console.WriteLine("Insira Apenas Números");
+            return InputTime();
+        }
+    }
+
     public void Clear()
     {
-        for (int i = 0; i < array.Length; i++)
+        for(int i = 0; i < posicoesPreenchidas.Count; i++)
         {
-            for (int j = 0; j < array[i].Length; j++)
-            {
-                array[i][j] = null;
-            }
+            Console.WriteLine($"Placa: {array[posicoesPreenchidas[i][0]][posicoesPreenchidas[i][1]].GetPlaca()}");
+            Console.WriteLine("Digite a Hora e o Minuto de Saída");
+            Console.Write("Hora: ");
+            int hora = InputTime();
+            Console.Write("Minuto: ");
+            int minuto = InputTime();
+            array[posicoesPreenchidas[i][0]][posicoesPreenchidas[i][1]].GetTicket().SetHoraSaida(new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day, hora, minuto, 0));
+            Console.WriteLine(array[posicoesPreenchidas[i][0]][posicoesPreenchidas[i][1]].GetTicket());
+            array[posicoesPreenchidas[i][0]][posicoesPreenchidas[i][1]] = null;
+            posicoesPreenchidas.RemoveAt(i);
+            Clear();
         }
+        return;
     }
 
     public bool Contains(Carro data)
     {
         int hash = Hashing(data);
-        int index = hash;
-        int i = 1;
-        while (array[index / array[0].Length][index % array[0].Length] != null)
+        int linha = hash % array.Length;
+        int coluna = hash;
+        int tentativa = 1;
+        int tentativaLinha = 1;
+        while (array[linha][coluna] != null)
         {
-            if (array[index / array[0].Length][index % array[0].Length].GetPlaca() == data.GetPlaca()){
-                return true;
+            try
+            {
+                if (array[linha][coluna].GetPlaca() == data.GetPlaca())
+                {
+                    return true;
+                }
             }
-            index = (hash + (i * i)) % (array.Length * array[0].Length);
-            i++;
-            if (i > array.Length * array[0].Length)
+            catch
             {
                 return false;
+            }
+            coluna = (hash + tentativa) % array[linha].Length;            
+            tentativa++;
+            if (tentativa > array[linha].Length)
+            {
+                tentativa = 1;
+                linha++;
+                tentativaLinha++;
+                if (tentativaLinha > array.Length)
+                {
+                    return false;
+                }
+                linha = linha % array.Length;
+                coluna = hash;
             }
         }
         return false;
@@ -80,6 +137,13 @@ public class MatrizHashing
             if (n < array[m].Length)
             {
                 array[m][n] = null;
+                foreach (int[] i in posicoesPreenchidas)
+                {
+                    if (i[0] == m && i[1] == n)
+                    {
+                        posicoesPreenchidas.Remove(i);
+                    }
+                }
                 count--;
             }
         }
@@ -88,21 +152,45 @@ public class MatrizHashing
     public void Remove(Carro data)
     {
         int hash = Hashing(data);
-        int index = hash;
-        int i = 1;
-        while (array[index / array[0].Length][index % array[0].Length] != null)
+        int linha = hash % array.Length;
+        int coluna = hash;
+        int tentativa = 1;
+        int tentativaLinha = 1;
+        while (array[linha][coluna] != null)
         {
-            if (array[index / array[0].Length][index % array[0].Length].GetPlaca() == data.GetPlaca())
+            try
             {
-                array[index / array[0].Length][index % array[0].Length] = null;
-                count--;
+                if (array[linha][coluna].GetPlaca() == data.GetPlaca())
+                {
+                    array[linha][coluna] = null;
+                    foreach (int[] i in posicoesPreenchidas)
+                    {
+                        if (i[0] == linha && i[1] == coluna)
+                        {
+                            posicoesPreenchidas.Remove(i);
+                            break;
+                        }
+                    }
+                    count--;
+                }
+            }
+            catch
+            {
                 return;
             }
-            index = (hash + (i * i)) % (array.Length * array[0].Length);
-            i++;
-            if (i > array.Length * array[0].Length)
+            coluna = (hash + tentativa) % array[linha].Length;            
+            tentativa++;
+            if (tentativa > array[linha].Length)
             {
-                return;
+                tentativa = 1;
+                linha++;
+                tentativaLinha++;
+                if (tentativaLinha > array.Length)
+                {
+                    return;
+                }
+                linha = linha % array.Length;
+                coluna = hash;
             }
         }
     }
@@ -115,19 +203,36 @@ public class MatrizHashing
     public int[] IndexOf(Carro data)
     {
         int hash = Hashing(data);
-        int index = hash;
-        int i = 1;
-        while (array[index / array[0].Length][index % array[0].Length] != null)
+        int linha = hash % array.Length;
+        int coluna = hash;
+        int tentativa = 1;
+        int tentativaLinha = 1;
+        while (array[linha][coluna] != null)
         {
-            if (array[index / array[0].Length][index % array[0].Length].GetPlaca() == data.GetPlaca())
+            try
             {
-                return new int[] { index / array[0].Length, index % array[0].Length };
+                if (array[linha][coluna].GetPlaca() == data.GetPlaca())
+                {
+                    return new int[] {linha, coluna};
+                }
             }
-            index = (hash + (i * i)) % (array.Length * array[0].Length);
-            i++;
-            if (i > array.Length * array[0].Length)
+            catch
             {
-                return new int[] { -1, -1 };
+                return new int[] {-1, -1};
+            }
+            coluna = (hash + tentativa) % array[linha].Length;
+            tentativa++;
+            if (tentativa > array[linha].Length)
+            {
+                tentativa = 1;
+                linha++;
+                tentativaLinha++;
+                if (tentativaLinha > array.Length)
+                {
+                    return new int[] { -1, -1 };
+                }
+                linha = linha % array.Length;
+                coluna = hash;
             }
         }
         return new int[] { -1, -1 };
